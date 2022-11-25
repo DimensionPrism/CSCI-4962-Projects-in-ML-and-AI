851199d782c8ac59728dac3da58bdb1d7fa622a4
DimensionPrism <liuf7@rpi.edu>
2022-11-25 18:38:11 -0500

    train_yolov5

--- Final Project/train_yolov5.py
+++ Final Project/train_yolov5.py
@@ -2,8 +2,8 @@ import os
 
 from roboflow import Roboflow
 from src.model.yolov5.train import parse_opt
-from src.model.yolov5.train import main as yolo_train
+from src.model.yolov5.train import main as train_yolov5
 
 if __name__ == "__main__":
     opt = parse_opt()
-    yolo_train(opt)
+    train_yolov5(opt)
